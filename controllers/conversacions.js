a69a2c72cff6dfb3c18628e18edc2bdd919127fa
JuanBohorquezA <juanbohorquezaponte@gmail.com>
2023-11-25 11:54:47 -0500

    vercel

--- controllers/conversacions.js
+++ controllers/conversacions.js
@@ -43,7 +43,8 @@ router.get('/getConversations', middleware.verificarJWT, async (req, res) => {
                     });
                 }
             }
-            if(allConversations == null){
+            console.log("Conversaciones: ",conversations)
+            if(conversations == null){
                 return res.status(404).send({ message: "Conversations not found", status: 404, data: null });
             }
     
